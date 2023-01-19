c8c074ad4475cfc5e2cff4a02d12f9b20a5367f0
Sefensi <Sefensi_reload@mail.ru>
2023-01-19 21:02:22 +0500

    Ex_2

---
+++ Homework_1/Ex_2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 4
+Console.WriteLine("Введите a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите c");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if(a > b)
+{
+    if(a > c)
+    {
+        max = a; 
+    }
+    else
+    {
+        max = c;
+    }
+}
+else if(b > c)
+{
+    max = b;
+}
+else
+{
+    max = c;
+}
+Console.WriteLine($"наибольшее из трех - {max}");
