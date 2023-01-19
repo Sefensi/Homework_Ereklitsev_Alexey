cb3b6ce546fc4115b36e6aa7723d720e37e9bb59
Sefensi <Sefensi_reload@mail.ru>
2023-01-19 20:29:21 +0500

    Homework_1

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -1,14 +1,16 @@
-﻿Задача 2
-int a = Console.Readline();
-int b = Console.Readline();
-
+﻿//Задача 2
+Console.WriteLine("Введите a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
 if (a > b)
 {
-int max = a
+    max = a;
 }
 else 
 {
-int max = b
+    max = b;
 }
 Console.Write("max = ");
 Console.Write(max);
