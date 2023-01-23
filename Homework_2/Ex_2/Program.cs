7b070258039ffba8bb20e5a3a1ff1266adbddb84
Sefensi <sefensi_reload@mail.ru>
2023-01-23 21:31:16 +0500

    H2_Ex2

---
+++ Homework_2/Ex_2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 13
+/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.*/
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+if(number.Length < 3)
+{
+    Console.WriteLine("Число двузначное");
+}
+else
+{
+    char third_n = number[2];
+    Console.WriteLine(third_n);
+}
