1894cfe64afeff3005f65c046d6768033abfb8e6
Sefensi <sefensi_reload@mail.ru>
2023-01-23 21:06:11 +0500

    H2_Ex1

---
+++ Homework_2/Ex_1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 10
+/*Напишите программу, которая принимает на вход трехзначное число и 
+на выходе показывает вторую цифру этого числа.*/
+
+Console.WriteLine("Введите трехзначное число");
+string number = Console.ReadLine();
+char second_n = number[1];
+Console.WriteLine(second_n);
