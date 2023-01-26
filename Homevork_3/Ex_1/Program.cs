60b3ad49029b6595e4532f966dc92b96da662132
Sefensi <sefensi_reload@mail.ru>
2023-01-27 00:12:53 +0500

    H3_Ex1

---
+++ Homevork_3/Ex_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 19
+/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.*/
+// 1234321
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+string palindrom(string number)
+{
+    for (int i = 0; i < (number.Length) / 2; i++)
+    {
+        char first = number[i];
+        char second = number[number.Length - i - 1];
+        if (first != second)
+        {
+            return "Число не является палиндромом";
+        }
+    }
+    return "число явдяется палиндромом";
+}
+
+Console.WriteLine(palindrom(number));
